14a46f6d1369a7d1d70091ce48052b30c4d8ea59
PriyaWebDesigner <naskartanu@111gmail.com>
2024-08-04 18:17:24 +0530

    Javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// // concatanation  
+
+let firstName = 'Priya';
+let lastName = 'Naskar';
+
+console.log('My name is '+firstName+' '+lastName);
+
+// Index Array.....
+
+let marks = [80 , 85 ,90 ,75, 95, 79, 80, 90, 75, 95];
+
+ console.log('The value in position 1='+ ' '+marks[0]);
+ console.log('The value in position 2='+ ' '+marks[1]);
+ console.log('The value in position 2='+ ' '+marks[2]);
+ console.log('The value in position 2='+ ' '+marks[3]);
+ console.log('The value in position 2='+ ' '+marks[4]);
+ console.log('The value in position 2='+ ' '+marks[5]);
+ console.log('The value in position 2='+ ' '+marks[6]);
+ console.log('The value in position 2='+ ' '+marks[7]);
+ console.log('The value in position 2='+ ' '+marks[8]);
+ console.log('The value in position 2='+ ' '+marks[9]);
+ console.log('The value in position 2='+ ' '+marks[10]);
+
+// // Associative Array(1D Array)....
+
+let car = {name:'Maruti Suzuki' , color:'white' , price:'20lac' , height:60 , width:40};
+
+console.log('The car name is '+car.name);
+console.log('The color is '+car.color);
+console.log('The car price is ='+ ' '+car.price);
+console.log('Height is '+car.height);
+console.log('width is '+car.width);
+
+
+let human = {firstName:'Rik' , lastName:'Naskar' , age:'22' , weight:70 , height:6.3};
+
+console.log('The Name is '+human.firstName+' '+human.lastName);
+console.log(human);
+
+// // Two Dimentional Array(2D Array).....
+
+let twoDimentional = [
+    [12, 5, 7, 9, 14],
+    [15, 9, 13, 7, 8],
+    [19, 2, 3, 6, 10],
+]
+
+console.log(twoDimentional[1][2]);
+console.log(twoDimentional[2][3]);
+console.log(twoDimentional[1][4]);
+console.log(twoDimentional[0][3]);
